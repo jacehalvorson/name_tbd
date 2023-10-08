b24852ef243b7efc994b731908f54408a210eb03
Buhlahkay0 <blkgbrl@gmail.com>
2023-10-08 09:45:10 -0500

    Created ThemeColor class

---
+++ lib/src/theme.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ThemeColor {
+  static const Color text = Color(0xFFFFFFFF);
+  static const Color background = Color(0xFF111313);
+  static const Color primary = Color(0xFF50A2D2);
+  static const Color disabledText = Color(0xFF616161);
+  static const Color darkElement = Color(0xFF616161);
+  static const Color darkGray = Color(0xFF292929);
+}
