340c2809ec4afa30b5fe332e948c74ad70e63c21
Buhlahkay0 <blkgbrl@gmail.com>
2023-10-10 14:14:22 -0500

    checkpoint incase I break everything

--- lib/src/sample_feature/draggable_sheet.dart
+++ lib/src/sample_feature/draggable_sheet.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:refresh/src/theme.dart';
+import 'searchbar.dart';
 
 class DraggableSheet extends StatefulWidget {
   const DraggableSheet({Key? key});
@@ -9,6 +10,20 @@ class DraggableSheet extends StatefulWidget {
 }
 
 class _DraggableSheetState extends State<DraggableSheet> {
+  final DraggableScrollableController _scrollableController =
+      DraggableScrollableController();
+
+  final double openSize = 0.8;
+  final double closeSize = 0.1;
+
+  void _openSheet() {
+    _scrollableController.animateTo(
+      openSize,
+      duration: const Duration(milliseconds: 300),
+      curve: Curves.easeInOut,
+    );
+  }
+
   final List<String> emojis = ["🏂", "⛳️", "🧑‍💻", "🎮", "🏀", "🏋️"];
   final List<String> names = [
     "Snowboarding",
@@ -22,23 +37,53 @@ class _DraggableSheetState extends State<DraggableSheet> {
   @override
   Widget build(BuildContext context) {
     return DraggableScrollableSheet(
-      initialChildSize:
-          0.1, // Initial size when collapsed (10% of the screen height)
-      minChildSize: 0.1, // Minimum size when collapsed
-      maxChildSize: 0.8, // Maximum size when fully expanded
+      controller: _scrollableController, // Pass the controller
+      initialChildSize: closeSize,
+      minChildSize: closeSize,
+      maxChildSize: openSize,
       snap: true,
       builder: (BuildContext context, ScrollController scrollController) {
-        return Container(
-          color: Colors.white,
-          child: ListView.builder(
-            controller: scrollController,
-            itemCount: 100,
-            itemBuilder: (BuildContext context, int index) {
-              return ListTile(
-                title: Text('Item $index'),
-              );
-            },
-          ),
+        return Expanded(
+              child: Container(
+                color: Colors.white,
+                child: Stack(
+                  children: [
+                    Row(
+                      children: [
+                        Expanded(
+                          child: Container(
+                            height: 48.0, // Fixed height
+                            color: Colors.blue, // Background color
+                            // Other properties and widgets you want in the container
+                          ),
+                        ),
+                        IconButton(
+                          icon: Icon(
+                            Icons.add_circle_outline,
+                            size: 32,
+                            color: ThemeColor.primary,
+                          ),
+                          onPressed: () {
+                            _openSheet();
+                          },
+                        ),
+                      ],
+                    ),
+                    Expanded(
+                      child: ListView.builder(
+                        controller: scrollController,
+                        itemCount: 100,
+                        itemBuilder: (BuildContext context, int index) {
+                          return ListTile(
+                            title: Text('Item $index'),
+                          );
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+         
         );
       },
     );
